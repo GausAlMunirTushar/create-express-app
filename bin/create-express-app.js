b95f03355365ee06b94c9658ec78f0e13c582b3f
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-10-31 15:14:08 +0600

    chore(cli): enhance command handling with error catcher, dynamic version, and improved help output

--- bin/create-express-app.js
+++ bin/create-express-app.js
@@ -7,9 +7,7 @@ const program = new Command();
 
 program
 	.name('create-ex-app')
-	.description(
-		'CLI to generate Express.js projects in JavaScript or TypeScript',
-	)
+	.description('Generate Express.js apps in JavaScript or TypeScript')
 	.version('1.0.1');
 
 program
